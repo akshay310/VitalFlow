f271610e5caf6b21498675b702aa865da54bc87d
akshay310 <70632287+akshay310@users.noreply.github.com>
2024-06-16 22:03:44 +0530

    Add files via upload

---
+++ whydonateblood.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+
+<?php 
+$active ='why';
+include('header.php');
+?>
+
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+  <div class="container">
+  <div id="content-wrap" style="padding-bottom:50px;">
+<div class="row">
+    <div class="col-lg-6">
+        <h1 class="mt-4 mb-3">Why Should I Donate Blood ? </h1>
+        <p>Blood, our body's red river, is life's essential delivery system. It carries oxygen and nutrients, whisks away waste, and fights infection – all thanks to its amazing components: red blood cells, white blood cells, and platelets.</p>
+        <p>
+        Donating blood is a simple act that can save lives. Every two seconds in the India, someone needs blood. Blood cannot be manufactured, it can only come from generous donors like you. A single donation can help up to three people. Your blood donation is especially important for people with cancer, trauma victims, and surgical patients. You can donate blood every 56 days. Donating blood is safe and easy. Be a hero - donate blood today!
+      </p>
+      <p> Every donation is vital because blood has a short shelf life. There's a constant need for replenishment. Donating offers a free mini-health check, letting you know your iron levels and overall health. It even stimulates your body to produce new red blood cells, potentially boosting your own health!  Join the countless heroes who save lives through blood donation. It's a small effort with a massive impact.</p>
+
+    </div>
+    <div class="col-lg-6">
+        <img class="img-fluid rounded" src="savelife.jpg" style="height:600px; width:1000px" alt="error"  >
+    </div>
+</div>
+</div>
+
+</div>
+
+<?php include('footer.php')
+?>
+</div>
+</body>
+
+</html>
